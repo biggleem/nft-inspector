89bc5086ecee7306a18e4a6396fd4563676d96ee
kalim hasan <biggleem@gmail.com>
2021-10-17 17:44:55 -0400

    updated nft-inspector, sorry for the delay working on the rarity portion.

--- nft.py
+++ nft.py
@@ -1,72 +1,43 @@
 import streamlit as st
 import requests, json
-import pandas as pd
 from web3 import Web3
+import pandas as pd
+from PIL import Image
 
-endpoints = st.sidebar.selectbox("Endpoints", ['Assets', 'Events', 'Rarity'])
+image = Image.open('images/nft-inspector-logo.gif')
+st.sidebar.image(image, caption='Inspect any OpenSea.io NFT collection.')
+st.sidebar.header("OpenSea API Endpoints")
+endpoint_choices = ['Assets', 'Events', 'Rarity']
+endpoint = st.sidebar.selectbox("Choose an Endpoint", endpoint_choices)
 
-st.header(f"Open Sea NFT Inspector - {endpoints}")
+st.title(f"OpenSea NFT Inspector - {endpoint}")
 
-# st.sidebar.subheader("Filters")
+def render_asset(asset):
+    if asset['name'] is not None:
+        st.subheader(asset['name'])
+    else:
+        st.subheader(f"{asset['collection']['name']} #{asset['token_id']}")
 
-# collection = st.sidebar.text_input("Collection")
-# owner = st.sidebar.text_input("Owner")
+    if asset['description'] is not None:
+        st.write(asset['description'])
+    else:
+        st.write(asset['collection']['description'])
 
-if endpoints == 'Assets':
-    st.sidebar.header('Filters')
-    owner = st.sidebar.text_input("Owner")
-    collection = st.sidebar.text_input("Collection")
-    params = {'owner': owner}
-    if collection:
-        params['collection'] = collection
-    
-    r = requests.get('https://api.opensea.io/api/v1/assets', params=params)
-
-    assets = r.json()['assets']
-    for assets in assets:
-        # render_asset(asset)
-    
-    st.subheader("Raw JSON Data")
-    st.write(r.json())
-
-    if endpoints == 'Rarity':
-        with open(assets.json) as f:
-            data = json.loads(f.read())
-            asset_rarities = []
+    if asset['image_url'].endswith('mp4') or asset['image_url'].endswith('mov'):
+        st.video(asset['image_url'])
+    elif asset['image_url'].endswith('svg'):
+        svg = requests.get(asset['image_url']).content.decode()
+        st.image(svg)
+    elif asset['image_url']:
+        st.image(asset['image_url'])
 
 
-
-# if endpoints == 'Assets':
-
-#    params = {}
-
-#    if collection:
-#         params['collection'] = collection
-#    if owner:
-#         params['owner'] = owner
-# r = requests.get("https://api.opensea.io/api/v1/assets", params=params)
-
-# response = r.json()
-
-# for asset in response["assets"]:
-#     if asset['name']:
-#         st.write(asset['name'])
-#     else:
-#         st.write(f"{asset['collection']['name']} #{asset['token_id']}")
-#     if asset['image_url'].endswith('mp4') or asset['image_url'].endswith('mov'):
-#         st.video(asset['image_url'])
-#     else:
-#         st.image(asset['image_url'])
-
-
-if endpoints == 'Events':
-    collection = st.sidebar.text_input("Collection")
-    asset_contract_address = st.sidebar.text_input("Contact Address")
+if endpoint == 'Events':
+    collection = st.sidebar.text_input("Collection Slug")
+    asset_contract_address = st.sidebar.text_input("Contract Address")
     token_id = st.sidebar.text_input("Token ID")
     event_type = st.sidebar.selectbox("Event Type", ['offer_entered', 'cancelled', 'bid_withdrawn', 'transfer', 'approve'])
-
     params = {}
-
     if collection:
         params['collection_slug'] = collection
     if asset_contract_address:
@@ -79,9 +50,7 @@ if endpoints == 'Events':
     r = requests.get('https://api.opensea.io/api/v1/events', params=params)
 
     events = r.json()
-    
     event_list = []
-
     for event in events['asset_events']:
         if event_type == 'offer_entered':
             if event['bid_amount']:
@@ -95,7 +64,52 @@ if endpoints == 'Events':
 
     df = pd.DataFrame(event_list, columns=['time', 'bidder', 'bid_amount', 'collection', 'token_id'])
     st.write(df)
-
+    st.subheader("Raw JSON Data")
     st.write(events)
- 
+
+if endpoint == 'Assets':
+    st.sidebar.header('Filters')
+    owner = st.sidebar.text_input("Owner Address")
+    collection = st.sidebar.text_input("Collection Slug")
+    params = {'owner': owner}
+    if collection:
+        params['collection'] = collection
+
+    r = requests.get('https://api.opensea.io/api/v1/assets', params=params)
+
+    assets = r.json()['assets']
+    for asset in assets:                
+        render_asset(asset)
+
+    st.subheader("Raw JSON Data")
     st.write(r.json())
+
+if endpoint == 'Rarity':
+    with open('assets.json') as f:
+        data = json.loads(f.read())
+        asset_rarities = []
+
+        for asset in data['assets']:
+            asset_rarity = 1
+
+            for trait in asset['traits']:
+                trait_rarity = trait['trait_count'] / 8888
+                asset_rarity *= trait_rarity
+
+            asset_rarities.append({
+                'token_id': asset['token_id'],
+                'name': f"Wanderers {asset['token_id']}",
+                'description': asset['description'],
+                'rarity': asset_rarity,
+                'traits': asset['traits'],
+                'image_url': asset['image_url'],
+                'collection': asset['collection']
+            })
+
+        assets_sorted = sorted(asset_rarities, key=lambda asset: asset['rarity']) 
+
+        for asset in assets_sorted[:20]:
+            render_asset(asset)
+            st.subheader(f"{len(asset['traits'])} Traits")
+            for trait in asset['traits']:
+                st.write(f"{trait['trait_type']} - {trait['value']} - {trait['trait_count']} have this")
